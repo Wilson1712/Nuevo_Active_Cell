9ba0a0958f2abb63c7b3ab78e377e9df9e414202
Cami <camilaaguirre996@gmail.com>
2019-11-17 22:47:53 -0500

    config

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+const LBS = 'Librerias/';
+const VIEWS = 'Vistas/';
+define('RQ', 'Resource/');
+define('URL', 'http://localhost/php/Nuevo_Active_Cell/');
+?>
---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+$url = isset($_GET['url']) ? $_GET['url']:"Index/index";
+$url = explode("/", $url);
+$controller = "";
+$method = "";
+if(isset($url[0])){
+    $controller = $url[0];
+}
+if(isset($url[1])){
+    if($url[1] != ''){
+        $method =$url[1];
+    }
+}
+spl_autoload_register(function($class){
+
+});
+echo $controller."-----".$method;
+?>
