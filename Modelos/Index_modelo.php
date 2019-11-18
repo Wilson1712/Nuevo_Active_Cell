4bff3e96d1bb481abcd7ab0643f6fa1f200707fd
Wilson1712 <54631207+Wilson1712@users.noreply.github.com>
2019-11-18 11:22:01 -0500

    mvc

---
+++ Modelos/Index_modelo.php
@@ -0,0 +1,11 @@
+<?php
+    class Index_modelo{
+        function __construct(){
+            $this->indexmodelo();
+        }
+
+        function indexmodelo(){
+            echo "Metodo Modelo ";
+        }
+    }
+?>
